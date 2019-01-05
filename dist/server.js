0b01d0780d1ab6234f04083a1f5dd4952d64fffd
RinatRezyapov <rinat.rezyapov@gmail.com>
2019-01-05 15:27:03 +0500

    check5

--- dist/server.js
+++ dist/server.js
@@ -2,17 +2,23 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const configMongoose_1 = require("./config/configMongoose");
 const configPassport_1 = require("./config/configPassport");
-const configApp_1 = require("./config/configApp");
 const configRouter_1 = require("./config/configRouter");
 const configWebSocket_1 = require("./config/configWebSocket");
 const port = process.env.PORT || 80;
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+exports.SECRET = 'server secret';
+exports.TOKENTIME = 120 * 60;
 configMongoose_1.mongooseConnect();
-const server = configApp_1.configApp();
 const passport = configPassport_1.configPassport();
 const router = configRouter_1.configRouter(passport);
-server.use(passport.initialize());
-server.use('/', router);
-server.listen(port, () => console.log(`Listening on ${port}`));
+const server = express()
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(cors())
+    .use(passport.initialize())
+    .use('/', router)
+    .listen(port, () => console.log(`Listening on ${port}`));
 configWebSocket_1.configWebSocket(server);
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -4,17 +4,26 @@ import { configApp } from './config/configApp';
 import { configRouter } from './config/configRouter';
 import { configWebSocket } from './config/configWebSocket';
 const port = process.env.PORT || 80;
-const express = require('express');
+
+
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+export const SECRET = 'server secret';
+export const TOKENTIME = 120 * 60;
 
 mongooseConnect();
 
-const server = configApp();
 const passport = configPassport();
 const router = configRouter(passport);
 
-server.use(passport.initialize());
-server.use('/', router);
+const server = express()
+.use(bodyParser.json())
+.use(bodyParser.urlencoded({ extended: false }))
+.use(cors())
+.use(passport.initialize())
+.use('/', router)
+.listen(port, () => console.log(`Listening on ${ port }`))
 
-server.listen(port, () => console.log(`Listening on ${ port }`));
 
 configWebSocket(server);
