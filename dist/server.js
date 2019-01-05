4641fd63af89c4d6a0e9cb25b47e92c3966c10ba
RinatRezyapov <rinat.rezyapov@gmail.com>
2019-01-05 15:17:17 +0500

    check2

--- dist/server.js
+++ dist/server.js
@@ -1,17 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const configMongoose_1 = require("./config/configMongoose");
-const configPassport_1 = require("./config/configPassport");
-const configApp_1 = require("./config/configApp");
-const configRouter_1 = require("./config/configRouter");
 const configWebSocket_1 = require("./config/configWebSocket");
 const port = process.env.PORT || 80;
+const express = require('express');
 configMongoose_1.mongooseConnect();
-const app = configApp_1.configApp();
-const passport = configPassport_1.configPassport();
-const router = configRouter_1.configRouter(passport);
+/*const app = configApp();
+const passport = configPassport();
+const router = configRouter(passport);
+
 app.use(passport.initialize());
 app.use('/', router);
-app.listen(port);
-configWebSocket_1.configWebSocket(app);
+app.listen(port);*/
+const server = express()
+    .listen(port, () => console.log(`Listening on ${port}`));
+configWebSocket_1.configWebSocket(server);
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,19 @@ import { configApp } from './config/configApp';
 import { configRouter } from './config/configRouter';
 import { configWebSocket } from './config/configWebSocket';
 const port = process.env.PORT || 80;
+const express = require('express');
 
 mongooseConnect();
 
-const app = configApp();
+/*const app = configApp();
 const passport = configPassport();
 const router = configRouter(passport);
 
 app.use(passport.initialize());
 app.use('/', router);
-app.listen(port);
+app.listen(port);*/
 
-configWebSocket(app);
+const server = express()
+  .listen(port, () => console.log(`Listening on ${ port }`));
+
+configWebSocket(server);
